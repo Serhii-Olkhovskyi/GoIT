54671466500f9f10db376eaf979317db1816b7d5
Sergii_O <komkoraz@gmail.com>
2022-10-30 19:04:18 +0300

    1. Реализованы все классы из задания. 2. Записи Record в AddressBook хранятся как значения в словаре. В качестве ключей используется значение Record.name.value. 3. Record хранит объект Name в отдельном атрибуте. 4. Record хранит список объектов Phone в отдельном атрибуте. 5. Record реализует методы для добавления/удаления/редактирования объектов Phone. 6. AddressBook реализует метод add_record, который добавляет Record в self.data.

---
+++ GoIt_Home_Work_5/class_boot.py
@@ -0,0 +1,110 @@
+"""
+В модуле разработаны следующие классы:
+    AddressBook - создание адресной книги
+"""
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    """
+    Класс AddressBook, который наследуется от UserDict, и добавлена логика поиска по записям
+    """
+
+    def add_record(self, name, phone=None):
+        """
+        Добавление контакта в адресную книгу
+        """
+        name = Name(name)
+        phone = Phone(phone)
+        record = Record(name=name)
+        record.add(phone)
+        self.data[record.name.value] = record
+        print(f'add contact: {record.name.value} is completed')
+
+    def find_number(self, name):
+        """
+        Поиск номера по имени
+        """
+        if name in self.data:
+            print(self.data[name])
+        else:
+            print('contacts not found')
+
+    def change(self, name, new_phone):
+        """
+         Изменение контакта в адресной книге
+        """
+
+        if name in self.data:
+            record = self.data.get(name)  # -> Record:
+            record.remove(new_phone)
+
+
+class Field:
+    """
+    Класс Field, который будет родительским для всех полей
+    """
+
+    def __init__(self, value):
+        self.__value = value
+
+    @property
+    def value(self):
+        """
+        pass
+        """
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        """
+        pass
+        """
+        self.__value = new_value
+
+
+class Name(Field):
+    """
+    Создаем класс для имени контакта. Класс Name, обязательное поле с именем.
+    """
+
+    def __init__(self, value):
+        super().__init__(value)
+        self.value = value
+
+
+class Phone(Field):
+    """
+    Создаем класс для телефонного номера. Класс Phone, необязательное поле с телефоном
+    """
+
+    def __init__(self, value):
+        super().__init__(value)
+        self.value = value
+
+
+class Record:
+    """
+    Класс Record, который отвечает за логику добавления/удаления/редактирования необязательных полей
+    и хранения обязательного поля Name.
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.phones = []
+
+    def add(self, phone):
+        """
+        Метод для добавления объектов Phone
+        """
+        self.phones.append(phone)
+
+    def remove(self, phone):
+        """
+        Метод для редактирования объектов Phone
+        """
+        self.phones = []
+        self.add(Phone(phone))
+
+    def __repr__(self):
+        return f"{', '.join([phone.value for phone in self.phones])}"
